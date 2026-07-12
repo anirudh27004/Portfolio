c4b7fe31bb6f0f259c4e9a54297bc51bc8d4bc87
Anirudh Gupta <anirudh27004@gmail.com>
2026-07-12 19:26:23 +1000

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Welcome, Dock, Home } from '#components'
-import { Safari, Terminal, Resume, Finder, Text, Image, Contact, Photos } from '#windows';
+import { Safari, Terminal, Resume, Finder, Text, Image, Contact } from '#windows';
 import gsap from 'gsap';
 
 import { Draggable } from "gsap/Draggable";
@@ -20,7 +20,6 @@ const App = () => {
       <Text />
       <Image />
       <Contact />
-      <Photos />
       <Home />
     </main>
   )
--- src/windows/index.js
+++ src/windows/index.js
@@ -5,7 +5,6 @@ import Finder from '#windows/Finder.jsx'
 import Text from '#windows/Text.jsx'
 import Image from '#windows/Image.jsx'
 import Contact from '#windows/Contact.jsx'
-import Photos from '#windows/Photos.jsx'
 
 export {
     Terminal,
@@ -14,6 +13,5 @@ export {
     Finder,
     Text,
     Image,
-    Contact,
-    Photos
+    Contact
  };
